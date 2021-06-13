7b64e86f58417cf3f1e64efc8b33a4fdb6454edc
ryo.suzuki <ottopiza@gmail.com>
2021-06-13 04:03:15 +0000

    .

---
+++ human.rb
@@ -0,0 +1,16 @@
+
+require "./thinkable"
+class Human < Animal
+  
+  attr_accessor :hobby
+  
+  def initialize(name, age, hobby)
+    self.name = name
+    self.age = age
+    self.hobby = hobby
+  end
+  
+    include Thinkable
+
+
+end
